8b4da6fdb4679c0737bc47af82a0a6618cc4c37a
mo-aden <mohamedaden17@outlook.com>
2023-02-01 20:03:21 -0700

    modified the getData func using async func

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const renderCountry = function (data, className = '') {
         <h4 class="country__region">${data.region}</h4>
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
-        ).toFixed(1)} people</p>
+        ).toFixed(1)}M population</p>
             <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
             <p class="country__row"><span> 🏢</span>${data.capital[0]}</p>
             </div>
@@ -30,42 +30,52 @@ const renderCountry = function (data, className = '') {
 };
 
 // Alternatively create a helper func
-const getJSON = function (url, errMessage = 'Something went wrong !!') {
-  return fetch(url).then(response => {
-    if (!response.ok)
-      throw new Error(renderErr(`${errMessage} ${response.status}`));
+// const getData = async function (url, errMessage = 'Something went wrong !!') {
+//   return fetch(url).then(response => {
+//     if (!response.ok)
+//       throw new Error(renderErr(`${errMessage} ${response.status}`));
 
-    return response.json();
-  });
-};
+//     return response.json();
+//   });
+// };
 
 const renderErr = function (err) {
   countriesContainer.insertAdjacentText('beforeend', err);
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
-    .then(data => {
-      renderCountry(data[0]);
+const getCountryAndNeighbour = async function (country) {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
 
-      const neighbouringCountries = data[0].borders[0];
+    if (!response.ok) throw new Error(`Country not found ... 🔥`);
 
-      console.log(neighbouringCountries);
-      if (!neighbouringCountries) return;
+    const data = await response.json();
 
-      return getJSON(
-        `https://restcountries.com/v3.1/alpha/${neighbouringCountries}`,
-        "Country doesn't have neighbouring country!"
-      );
-    })
-    .then(data => {
-      renderCountry(data[0], 'neighbour');
-    })
-    .catch(err => {
-      console.log(`Enter a differrent city or check the spelling ❤️‍🔥❤️‍🔥❤️‍🔥  `);
-      // alert(`Wrong country ...`);
+    // console.log(data[0]);
+    // render a card of the country searched
+    renderCountry(data[0]);
+
+    const neigbouringCountries = data[0].borders;
+
+    if (!neigbouringCountries) return;
+    console.log(neigbouringCountries);
+    //render the neighbouring Countries
+
+    neigbouringCountries.map(neighbour => {
+      fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+        .then(res => res.json())
+        .then(data2 => {
+          console.log(data2);
+
+          renderCountry(data2[0], 'neighbour');
+        });
     });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //uses the userInput to render a country
